ed95ff4dba28303ba0fb8e61d30b1ccbb81fad52
Salahuddin Rana <rana7cse@gmail.com>
2018-10-06 23:44:39 +0600

    virtual host added with symblink creation

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const readLine = require('readline-sync');
+const createSymlink = require('create-symlink');
+
 
 // config files;
-const nginxConfigStubDir = "./stubs/nginx.conf.stubs";
+const nginxConfigStubFile = "./stubs/nginx.conf.stubs";
 const fileContentType = 'utf8';
 let workdir = '/var/www/html';
-//site generation
+let outputdir = '/etc/nginx/sites-available';
+let symlinkTo = '/etc/nginx/sites-enabled';
+// site generation
 const site_name = readLine.question("Please enter your site name: ");
 const inFolder = readLine.question("Inside a folder add folder name (/): ");
 
-if (inFolder != ''){
+if (inFolder != '') {
     workdir = workdir + '/' + inFolder
 }
 
@@ -19,12 +23,18 @@ if(!fs.existsSync(workdir)){
     throw new Error(`Work directory (${workdir}) not exist for your site`);
 }
 
-console.log(workdir);
-
-const entry_point = readLine.question("Select an entry point (/): ");
-console.log(entry_point);
+let entry_point = readLine.question("Select an entry point (/): ");
+entry_point = (entry_point != '') ? workdir + "/" + entry_point : workdir;
 
 // read file from stubs
-fs.readFile(nginxConfigStubDir,fileContentType,function (err,fileContent) {
-
-});
+let stubContent = fs.readFileSync(nginxConfigStubFile,fileContentType);
+let outputFileName = outputdir+'/'+site_name;
+symlinkTo = symlinkTo+'/'+site_name;
+let configContent = stubContent
+    .replace(new RegExp(":site-name:",'g'),site_name)
+    .replace(":entry-point:",entry_point);
+let writeDir = fs.writeFileSync(outputFileName,configContent);
+createSymlink(outputFileName,symlinkTo)
+    .then(() => {
+        console.log("Successfully nginx config setup completed. Please restart nginx process");
+    });
